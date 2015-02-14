d44aa52c1f4b50296275ff5e2ab0a110e4a886a2
haroundemor <haroun.benhmida@outlook.com>
2015-02-14 19:31:20 +0100

    Add Synonyms & Hypernyms for English Language

--- js/popup.js
+++ js/popup.js
@@ -1,26 +1,54 @@
 jQuery(document).ready(function () {
     jQuery('#button').click(function () {
-        var word = $('#wordInput').val();
-
-
-        getWikiQuery(word, "def", -1, "en", function (data) {
-            $('#word').text(word);
-
-            var definitions = data.split('|');
-            var oList = $('<ol/>');
-            $.each(definitions, function (i, v) {
-                oList.append($('<li/>', {
-                    text: v
-                }));
-            });
-            $('#definitions').append(oList);
-        });
-
-        getWikiQuery(word, "pos", 1, "en", function (data) {
-            $('#nature').text(data);
-        });
+        requestAPI();
     });
+
+    jQuery(document).keypress(function (e) {
+        if (e.which == 13) {
+            requestAPI();
+        }
+    })
 });
 
 
-function printDefResult(data) {}
+function requestAPI() {
+    var word = $('#wordInput').val();
+    $('#word').text('');
+    $('#word').text(word);
+
+    getWikiQuery(word, "def", 5, "en", function (data) {
+        var definitions = data.split('|');
+        var oList = $('<ol/>');
+        $.each(definitions, function (i, v) {
+            oList.append($('<li/>', {
+                text: v
+            }));
+        });
+        $('#definitions').html('');
+        $('#definitions').append(oList);
+    });
+
+    getWikiQuery(word, "pos", 1, "en", function (data) {
+        $('#nature').text('');
+        $('#nature').text(data);
+    });
+
+    getWikiQuery(word, "syn", -1, "en", function (data) {
+        printSynonymsAndHypernyms(data, $('#synonyms'));
+    });
+
+    getWikiQuery(word, "hyper", -1, "en", function (data) {
+        printSynonymsAndHypernyms(data, $('#hypernyms'));
+    });
+}
+
+function printSynonymsAndHypernyms(data, container) {
+    var array = data.split('|');
+    var result = '';
+    $.each(array, function (i, v) {
+        result += v + ', ';
+    });
+    $('.sectionTitle').show();
+    container.text('');
+    container.text(result);
+}
